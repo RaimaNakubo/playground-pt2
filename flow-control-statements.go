50296cb1d1c4f0b53e02cdf6274c4dd0b77e228c
Nikita Belenkov <bna.gorzar@gmail.com>
2024-01-26 00:14:27 +0300

    A tour of Go n9: if-else, for statements and cases

--- flow-control-statements.go
+++ flow-control-statements.go
@@ -1,14 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+// if statement basic syntax
+func sqrt(x float64) string {
+	if x < 0 {
+		return sqrt(-x) + "i"
+	}
+	return fmt.Sprint(math.Sqrt(x))
+}
+
+// starting if with a short statement declaration
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	} else {
+		fmt.Printf("%g >= %g\n", v, lim)
+	}
+	return lim
+}
 
 func main() {
 
-	//basic syntax
+	//for loop basic syntax
 	sum := 0
 	for i := 0; i < 10; i++ {
 		sum += i
 	}
+	fmt.Println("for loop")
 	fmt.Println(sum)
 
 	//the init and post statements are optional, like C's "while" loop
@@ -23,4 +45,14 @@ func main() {
 
 	}
 	*/
+
+	fmt.Println("if statement")
+	fmt.Println(sqrt(2), sqrt(-4))
+
+	fmt.Println("if with a short statement")
+	fmt.Println(
+		pow(3, 2, 10),
+		pow(3, 3, 20),
+	)
+
 }
