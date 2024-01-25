d95df41393900f5b45992316f001ad29b3371bfc
Nikita Belenkov <bna.gorzar@gmail.com>
2024-01-26 02:35:54 +0300

    A tour of Go n11: switch cases and deferred calls

--- flow-control-statements.go
+++ flow-control-statements.go
@@ -2,37 +2,71 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"runtime"
+	"time"
 )
 
-// Sqrt implements square root function
-func Sqrt(x float64) float64 {
-	z := x / 2
-	buffer := float64(0)
-	change := float64(1)
-
-	fmt.Println("Calculating square root of", x)
-	for i := 0; i < 10; i++ {
-		buffer = z
-		z -= (z*z - x) / (2 * z)
-		change = buffer - z
-		fmt.Println("Iteration", i, ":", z, "change:", change)
-
-		if change == 0 {
-			fmt.Printf("Calculation completed in %v iterations\n", i+1)
-			break
-		}
-	}
-	return z
-}
-
-/*
-the z² − x above is how far away z² is from where it needs to be (x), and the division by 2z is the derivative of z²,
- to scale how much we adjust z by how quickly z² is changing. This general approach is called Newton's method.
- It works well for many functions but especially well for square root
-*/
-
 func main() {
-	fmt.Println("re-created square root is  ", Sqrt(122))
-	fmt.Println("standard lib square root is", math.Sqrt(122))
+	fmt.Print("Go runs on ")
+
+	//switch statement basic syntax
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		//any other OS
+		fmt.Printf("%s.\n", os)
+	}
+	fmt.Println()
+
+	//switch evaluation order
+	fmt.Println("Switch evaluation order:")
+	fmt.Println("When is Saturday?")
+	today := time.Now().Weekday()
+
+	switch time.Saturday {
+	case today + 0:
+		fmt.Println("Today.")
+	case today + 1:
+		fmt.Println("Tomorrow.")
+	case today + 2:
+		fmt.Println("In two days.")
+	default:
+		fmt.Println("Too far away.")
+	}
+	fmt.Println()
+
+	//Switch with no condition
+	fmt.Println("Switch with no condition:")
+	t := time.Now()
+
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Good Morning!")
+	case t.Hour() < 17:
+		fmt.Println("Good Afternoon!")
+	default:
+		fmt.Println("Good Evening.")
+	}
+	fmt.Println()
+
+	//deferred function call
+	fmt.Println("Deferred statement:")
+
+	/*
+		defer fmt.Println("forgor")
+		fmt.Println("I")
+
+		fmt.Println()
+	*/
+
+	//stacking deferred calls
+	fmt.Println("Counting:")
+
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+	fmt.Println("Done!")
 }
